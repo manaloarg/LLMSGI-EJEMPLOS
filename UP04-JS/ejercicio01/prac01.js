1f77cf8665add640da0fed26405c90803c91a33c
Manuel Alonso Argente <m.alonsoargente@edu.gva.es>
2026-02-09 11:33:53 +0100

    update

---
+++ UP04-JS/ejercicio01/prac01.js
@@ -0,0 +1,9 @@
+function intro(){
+    document.write("<h1>Introduccion a JavaScript (document.write)</h1>");
+}
+function holaMundo(){
+    window.alert("Hola, soy el alumno Manuel Alonso");
+}
+function fin(){
+    document.write("<h1>¡¡¡Hasta pronto!!! (document.write)</h1>");
+}

e7740270a4cf1f83504e1735f62c41c1570776de
Manuel Alonso Argente <m.alonsoargente@edu.gva.es>
2026-02-10 12:31:27 +0100

    update

--- UP04-JS/ejercicio05/prac05.js
+++ UP04-JS/ejercicio05/prac05.js
@@ -1,71 +1,13 @@
-function notaExamen() {
-    nota = parseInt(prompt("Introduzca la nota:"));
-    console.log(nota);
-
-    switch (nota) {
-        case 0:
-        case 1:
-        case 2:
-            document.getElementById("texto").innerHTML = "<p>0-3: Muy deficiente</p>";
-            break;
-        case 3:
-        case 4:
-            document.getElementById("texto").innerHTML = "3-5: Insuficiente";
-            break;
-        case 5:
-            document.getElementById("texto").innerHTML = "5-6: Suficiente";
-            break;
-        case 6:
-            document.getElementById("texto").innerHTML = "6-7: Bien";
-            break;
-        case 7:
-        case 8:
-            document.getElementById("texto").innerHTML = "7-9: Notable";
-            break;
-        case 9:
-        case 10:
-            document.getElementById("texto").innerHTML = "9-10: Excelente";
-            break;
-        default:
-            document.getElementById("texto").innerHTML = "No es un número o la nota es mayor de 10";
-            break;
-    }
-}
-
-/*
-function notaExamen() {
+function concatenar() {
+    let concatenado = "";
     do {
-        nota = parseInt(prompt("Introduzca la nota:"))
-    } while (nota<0 || nota>10 || isNaN(nota));
-    console.log(nota);
+        palabra = prompt("Introduzca las palabras()");
+        if (palabra != null) {
+            concatenado += palabra + " - ";
+            console.log(palabra);
+            console.log(concatenado);
+        }
+    } while (palabra != null);
 
-    switch (nota) {
-        case 0:
-        case 1:
-        case 2:
-            document.getElementById("texto").innerHTML = "<p>0-3: Muy deficiente</p>";
-            break;
-        case 3:
-        case 4:
-            document.getElementById("texto").innerHTML = "3-5: Insuficiente";
-            break;
-        case 5:
-            document.getElementById("texto").innerHTML = "5-6: Suficiente";
-            break;
-        case 6:
-            document.getElementById("texto").innerHTML = "6-7: Bien";
-            break;
-        case 7:
-        case 8:
-            document.getElementById("texto").innerHTML = "7-9: Notable";
-            break;
-        case 9:
-        case 10:
-            document.getElementById("texto").innerHTML = "9-10: Excelente";
-            break;
-        default:
-            document.getElementById("texto").innerHTML = "No es un número o la nota es mayor de 10";
-            break;
-    }
+    document.getElementById("texto").innerHTML = concatenado.substring(0,concatenado.length - 3);
 }
-*/

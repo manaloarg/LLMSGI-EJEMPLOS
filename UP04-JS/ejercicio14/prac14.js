b67a3f50bd91b4fd676fa6d0f1cc9bd2a3079e8b
Manuel Alonso Argente <m.alonsoargente@edu.gva.es>
2026-02-22 20:37:10 +0100

    update

--- UP04-JS/ejercicio14/prac14.js
+++ UP04-JS/ejercicio14/prac14.js
@@ -1,5 +1,16 @@
-function mayus() {
+function modificar() {
     texto = prompt("Introduzca el texto");
-
-    document.getElementById("texto").innerHTML = texto.toUpperCase();
+    array = texto.split("");
+    final = "";
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] === " ") {
+            final+="*";
+        } else {
+            final+=array[index];
+        }
+        if (index < array.length-1) {
+            final+="*";
+        }
+    }
+    document.getElementById("texto").innerHTML = final;
 }
